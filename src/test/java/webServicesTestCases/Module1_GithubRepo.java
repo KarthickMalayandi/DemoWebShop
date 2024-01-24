a29ab7a1649a60100e4accbf3605f3efe5763854
karth <karth@192.168.100.17>
2024-01-25 01:39:59 +0530

    BearerToken Updated

--- src/test/java/webServicesTestCases/Module1_GithubRepo.java
+++ src/test/java/webServicesTestCases/Module1_GithubRepo.java
@@ -9,7 +9,7 @@ import io.restassured.response.Response;
 
 public class Module1_GithubRepo extends BaseClass{
 
-	String bearerToken="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";
+	String bearerToken="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";
 	String baseURI="https://api.github.com";
 	String ownerName;
 	String repoName;
